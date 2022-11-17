c8fade4b9710b35c14e6d53e08d7f9c7234b5b49
hirabayashimei <k21rs108@st.kyusan-u.ac.jp>
2022-11-17 10:18:36 +0900

    Fetch Allボタンを追加

--- www/key.js
+++
@@ -1,3 +0,0 @@
-// This is a JavaScript file
-var appKey ="2ed2c8bb7343fd105606fa9fb252656f483d6c6accf99527949ef90582b76ed9";
-var clientKey ="9becf1cc6f0cfe0fa32d7fb78007d12c237546cb44fc371d4f0d3aae9860f51a";
